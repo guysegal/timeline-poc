87d6cae36b974e5720740df9f3c903332cd9de5e
Guy Segal <guysegal.il@gmail.com>
2017-05-16 12:56:57 +0300

    more hoc

--- src/invertedListView/timeline.js
+++ src/invertedListView/timeline.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {View, ListView} from 'react-native';
 import {connect} from 'react-redux';
-import {compose, lifecycle, mapProps, mapPropsStream} from 'recompose';
+import {compose, lifecycle, mapProps, mapPropsStream, withProps} from 'recompose';
 import uuid from 'react-native-uuid';
 import throttle from 'lodash.throttle';
 import InvertibleScrollView from 'react-native-invertible-scroll-view';
@@ -9,28 +9,50 @@ import InvertibleScrollView from 'react-native-invertible-scroll-view';
 import {withItemRenderFactory} from '../timelineItemsFactory';
 import {showFutureSection} from '../state';
 
+const withRefs = withProps({ refs: {} });
 
-
-const stopSrollAboveFuture = (Component) => props => {
+const withScrollToStopPosition = (Component) => props => {
     const VIEWPORT = 675
     const FUTURE_CARD_SIZE = 70
-    return <Component getScrollStopPosition={props.futureLength * FUTURE_CARD_SIZE - FUTURE_CARD_SIZE / 2} />
-    
+    return <Component {...props} scrollStopPosition={props.futureLength * FUTURE_CARD_SIZE - FUTURE_CARD_SIZE / 2} />
 }
 
-const enhance = compose(
-    connect(state => ({
-        items: state.items,
+const scrollToStopPositionOnFutureLengthChange = lifecycle({
+    componentWillUpdate(nextProps, nextState) {
+        if (nextProps.futureLength != this.props.futureLength) {
+            setTimeout(() => {
+                const scrollTo = this.props.refs[this.props.listViewRefName].scrollTo
+                scrollTo({y: this.props.scrollStopPosition || 0, animated: false})
+            }, 1); 
+        }                
+    }
+});
+
+const withPastItems = connect(state => ({items: state.items}));
+
+const withFutureItems = compose(
+    connect(state => ({       
         futureLength: state.futureLength || 5      
     })),
-    withItemRenderFactory,
     lifecycle({
         componentDidMount() {
             this.props.dispatch(showFutureSection(10));
         }
-    }),
-    stopSrollAboveFuture
+    })
+)
 
+const enhance = compose(
+    withRefs,
+    withProps({listViewRefName: "TimelineItemsViewer_ListView"}),
+    
+    withItemRenderFactory,
+        
+    withPastItems,
+    withFutureItems,
+    
+    withScrollToStopPosition,
+    scrollToStopPositionOnFutureLengthChange,
+    
 )
 
 class TimelineItemsViewer extends React.Component {
@@ -43,14 +65,6 @@ class TimelineItemsViewer extends React.Component {
           dataSource: this.ds.cloneWithRows(props.items, rowIds)
         };
     } 
-
-    componentWillUpdate(nextProps, nextState) {
-        if (nextProps.futureLength != this.props.futureLength) {
-            setTimeout(() => {
-                this.refs.itemsList.scrollTo({y: this.props.getScrollStopPosition(), animated: false})
-            }, 1); 
-        }
-    }
 
     _renderScrollComponent(props) {
         return <InvertibleScrollView {...props} inverted={true}  />;
@@ -70,7 +84,7 @@ class TimelineItemsViewer extends React.Component {
                 enableEmptySections
                 renderScrollComponent={this._renderScrollComponent.bind(this)}
                 renderRow={(item) => this.props.renderTimelineItem({item})}
-                ref='itemsList'
+                ref={r => this.props.refs[this.props.listViewRefName] = r}
                 scrollRenderAheadDistance={1500}
                 initialListSize={this.props.futureLength + 10} />)
     }
